7725a82ca85b2067101270161c12b10e0517916b
Laacis <j.lacis.code@gmail.com>
2022-11-14 10:24:33 +0100

    JS script now generates field matching the number of requested by the user in the form

--- load_planer/static/load_planer/delplanform.js
+++ load_planer/static/load_planer/delplanform.js
@@ -9,41 +9,84 @@ function create_form(form_field){
     console.log("creating form field");
     
     // delivery_id field
-    const del_id_f = document.createElement('textarea');
+    const del_id_f = document.createElement('input');
     del_id_f.id = "delivery_id";
+    del_id_f.type = 'text';
     form_field.appendChild(del_id_f);
 
     // Quarter field
-    const quater_f = document.createElement('input');
-    quater_f.type = "number";
-    quater_f.min = 1;
-    quater_f.max = 4;
+    const quater_f = document.createElement('select');
     form_field.appendChild(quater_f);
+    for ( var i = 1; i < 5; i++) {
+        var option = document.createElement('option')
+        option.value = i;
+        option.text = "Q"+i;
+        quater_f.appendChild(option);
+    }
     // Year field
-    const year_f = document.createElement('input');
-    year_f.type = "number";
-    year_f.min = 2022;
-    year_f.max = 2024;
+    const year_f = document.createElement('select');
     form_field.appendChild(year_f);
+    for ( var i = 2022; i < 2025; i++) {
+        var option = document.createElement('option')
+        option.value = i;
+        option.text = i;
+        year_f.appendChild(option);
+    }
 
-    // this is going to be transfered into JSON later :
-    // {"delivery order from 1 to n":"destination_id"}
-    // Delivery order field
-    const del_ord_f = document.createElement('input');
-    del_ord_f.type = "number";
-    del_ord_f.min = 1;
-    del_ord_f.max = 20;
-    form_field.appendChild(del_ord_f);
+    // Let user choose  number of destinations to generate input+texarea * number
+    const destination_choice = document.createElement('select');
+    form_field.appendChild(destination_choice);
+    for (var i = 1; i < 20; i++) {
+        var option = document.createElement('option');
+        option.value = i;
+        option.text = i;
+        destination_choice.appendChild(option);
+    }
+    const button_f_request = document.createElement('button');
+    button_f_request.classList = 'btn btn-secondary';
+    button_f_request.type = 'submit';
+    button_f_request.textContent = "Request form";
+    button_f_request.id = "request_field_btn";
+    form_field.appendChild(button_f_request);
+    button_f_request.addEventListener('click', function() {
+        console.log("clicked this button!");
+        const extra_fields = document.createElement('div');
+        extra_fields.id = "delivery_plan_extra_fields";
+        extra_fields.textContent = "extra field";
+        form_field.append(extra_fields);
+        console.log(`building extra ${destination_choice.value} fields`)
+        for (var i = 0; i < destination_choice.value; i++) {
+            const set_div = document.createElement('div');
+            set_div.id = `div_id:${i+1}`;
+            set_div.textContent = i+1+".";
+            extra_fields.appendChild(set_div)
+        }
+        //cleaning up button and choice field
+        button_f_request.remove();
+        destination_choice.remove();
+    });
+    
 
-    //destination_id field
-    const dest_id_f = document.createElement('textarea');
-    dest_id_f.id = "destination_id";
-    form_field.appendChild(dest_id_f);
+    // // this is going to be transfered into JSON later :
+    // // {"delivery order from 1 to n":"destination_id"}
+    // // Delivery order field
+    // const del_ord_f = document.createElement('input');
+    // del_ord_f.type = "number";
+    // del_ord_f.min = 1;
+    // del_ord_f.max = 20;
+    // form_field.appendChild(del_ord_f);
 
-    // buttor field
-    const button = document.createElement('button');
-    button.classList = 'btn btn-secondary';
-    button.type = 'submit';
-    button.textContent = "Create";
-    form_field.appendChild(button);
+
+            // //destination_id field
+            // const dest_id_f = document.createElement('textarea');
+            // dest_id_f.id = "destination_id";
+            // form_field.appendChild(dest_id_f);
+
+
+    // // buttor field
+    // const button = document.createElement('button');
+    // button.classList = 'btn btn-secondary';
+    // button.type = 'submit';
+    // button.textContent = "Create";
+    // form_field.appendChild(button);
 }
